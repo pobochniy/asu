9662b2da0063c583ba8e83cec48d2210fca771e1
Pobochniy <pobochniy@mjr.ru>
2019-03-31 19:45:49 +0300

    angular add login module

---
+++ Web/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/api/auth.service';
+import { UserService } from '../shared/core/user.service';
+import { loginFormModel } from '../shared/form-models/login-form.model';
+
+@Component({
+  selector: 'login-auth',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  providers: [AuthService, UserService]
+})
+export class LoginComponent {
+  public loginForm = loginFormModel;
+
+  constructor(private service: AuthService
+    , private router: Router
+    , private userService: UserService) { }
+
+  async onSubmit() {
+    try {
+      let usr = await this.service.login(this.loginForm);
+      this.userService.User = usr;
+      this.router.navigateByUrl('/chat');
+    }
+    catch {
+      alert("не подходит");
+    }
+  }
+}
