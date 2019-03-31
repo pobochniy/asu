6fcb905b4b0d7eb9db7da55a7db21cf42214d4f8
Pobochniy <pobochniy@mjr.ru>
2019-03-31 19:46:19 +0300

    fix AuthController, fix app.module

--- Web/ClientApp/src/app/app.module.ts
+++ Web/ClientApp/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HomeComponent } from './home/home.component';
+import { LoginModule } from './login/login.module';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { ChatModule } from './shared/chat/chat.module';
+import { UserModel } from './shared/models/user.model';
+
 
 @NgModule({
   declarations: [
@@ -24,13 +26,14 @@ import { ChatModule } from './shared/chat/chat.module';
     HttpClientModule,
     FormsModule,
     ChatModule,
+    LoginModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [],
+  providers: [UserModel],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
