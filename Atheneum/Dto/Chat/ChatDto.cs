681bc604a7fd61901ad266bbe9f55a04d53830ce
Pobochniy <pobochniy@mjr.ru>
2019-05-02 14:12:38 +0300

    ChatDto add type

--- Atheneum/Dto/Chat/ChatDto.cs
+++ Atheneum/Dto/Chat/ChatDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Atheneum.Dto.Chat
+﻿using Atheneum.Enums;
+
+namespace Atheneum.Dto.Chat
 {
     public class ChatDto : PushChatDto
     {
         public string Id { get; set; }
+
+        public ChatTypeEnum Type { get; set; }
 
         public string Login { get; set; }
     }
