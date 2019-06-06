80bc3cc32968c32d3f6b5f8b6eb8940594a8f944
polagaev <polagaev@mjr.ru>
2019-06-06 13:54:36 +0300

    add rerizer

---
+++ Web/ClientApp/src/app/shared/chat/chat-resizer.directive.ts
@@ -0,0 +1,51 @@
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+
+@Directive({
+  selector: '[appChatResizer]'
+})
+export class ChatResizerDirective implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('resizePanel') panel: ElementRef;
+  private el: any;
+  private isDragabble = false;
+  private startHeight = 0;
+  private startHeightPosition = 0;
+
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.el = document.getElementById('resizePanel');
+    this.el.addEventListener('mousedown', this.onDown.bind(this));
+    this.el.addEventListener('mouseup', this.onUp.bind(this));
+    document.addEventListener('mousemove', this.onMove.bind(this));
+  }
+
+  ngOnDestroy() {
+    this.el.removeEventListener('mousedown');
+    this.el.removeEventListener('mouseup');
+  }
+  
+  onDown(e: MouseEvent) {
+    this.isDragabble = true;
+  }
+  
+  onUp(e) {
+    this.isDragabble = false;
+    this.startHeight = (this.element.nativeElement as HTMLElement).offsetHeight;
+    this.startHeightPosition = e.pageY;
+  }
+
+  onMove(e) {
+    if (this.isDragabble) {
+      console.log(e.pageY);
+      (this.element.nativeElement as HTMLElement).style.height = this.startHeight + this.startHeightPosition - e.pageY + 'px';
+    }
+  }
+
+}
