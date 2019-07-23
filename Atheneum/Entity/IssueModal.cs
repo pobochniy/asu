db131fc3cfe3fdfef7a7b505479c358ebaed69b9
Vaanren <vaanren-it@mail.ru>
2019-07-23 18:26:59 +0300

    Описание формы Create Issue

---
+++ Atheneum/Entity/IssueModal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Atheneum.Enums;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Atheneum.Entity.Identity
+{
+    public class IssueModal
+    {
+        [Key]
+        public long IssueID { get; set; } 
+
+        public string Director { get; set; }        
+
+        public string Executor { get; set; }        
+
+        public string Reporters { get; set; }       
+
+        public string Summary { get; set; }         
+
+        public string Description { get; set; }     
+
+
+        public IssueTypeEnum Type { get; set; }  
+
+        public IssueStatusEnum Status { get; set; } 
+
+        public IssuePriorityEnum Priority { get; set; } 
+
+
+        public int DirectorEstimatedTime { get; set; }
+
+        public int ExecutorEstimatedTime { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime DeadlineDate { get; set; }
+
+        public int TimeSpent { get; set; }
+
+        
+        public string Attachment { get; set; }
+
+        public string EpicLink { get; set; }
+
+        public string OtherLinks { get; set; }
+    }
+
+    public class IssueConfiguration : IEntityTypeConfiguration<IssueModal>
+    {
+        
+        public void Configure(EntityTypeBuilder<IssueModal> builder)
+        {
+            builder
+               .HasIndex(u => u.IssueID)
+               .IsUnique();     
+        }
+            
+    }
+} 
---
+++ Atheneum/Enums/IssueTypeEnum.cs
@@ -0,0 +1,30 @@
+﻿namespace Atheneum.Enums
+
+
+{
+    public enum IssueTypeEnum 
+    {
+        story,
+        task,
+        bug,
+        knowledge,
+        meeting
+    }
+
+    public enum IssueStatusEnum
+    {
+        todo,
+        inwork,
+        done,
+        testing,
+        resolve
+    }
+
+    public enum IssuePriorityEnum
+    {
+        low,
+        middle,
+        high,
+        highest
+    }
+}
