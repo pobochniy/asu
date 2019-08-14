18dbecc0e660649efb6e94760ee05ec5f5e2603a
Vaanren <vaanren-it@mail.ru>
2019-08-14 22:43:42 +0300

    Issue API

---
+++ Atheneum/Interface/IIssuePage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atheneum.Interface
+{
+    public interface IIssuePage
+    {
+    }
+}
--- Atheneum/Migrations/20190809133058_issues.Designer.cs
+++ Atheneum/Migrations/20190809133058_issues.Designer.cs
@@ -4,14 +4,16 @@ using Atheneum.Entity.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Atheneum.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190809133058_issues")]
+    partial class issues
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
