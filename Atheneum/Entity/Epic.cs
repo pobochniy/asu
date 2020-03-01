32ad6aa7395037aa4a2f4d3eaa4f6506797fe877
ValMorgunov <48404445+ValMorgunov@users.noreply.github.com>
2020-03-01 21:49:28 +0300

    Add Epic.cs

---
+++ Atheneum/Entity/Epic.cs
@@ -0,0 +1,20 @@
+﻿using Atheneum.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Atheneum.Entity.Identity
+{
+    public class Epic
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid? Reporter { get; set; }
+        public IssuePriorityEnum PriorityEnum { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
+
