4697973745e6628fba0068a3fd4da4343fbc27ca
Pobochniy <pobochniy@mjr.ru>
2020-03-20 15:15:26 +0300

    fix

--- Web/ClientApp/src/app/shared/models/chat.model.ts
+++ Web/ClientApp/src/app/shared/models/chat.model.ts
@@ -2,14 +2,14 @@ import { ChatTypeEnum } from "../enums/chat-type.enum";
 import { PushChatModel } from "./push-chat.model";
 
 export class ChatModel extends PushChatModel {
-  public Id: string;
+  public id: string;
   public type: ChatTypeEnum;
   public login: string;
 
   constructor(data: any) {
     super("");
 
-    this.Id = data.id != undefined ? data.id : "";
+    this.id = data.id != undefined ? data.id : "";
     this.login = data.login != undefined ? data.login : "";
     this.message = data.message != undefined ? data.message : "";
     this.privat = data.privat != undefined ? data.privat : [];
@@ -18,7 +18,7 @@ export class ChatModel extends PushChatModel {
   }
 
   public getTime(): string {
-    let ticks = parseInt(this.Id);
+    let ticks = parseInt(this.id);
     let ticksToMicrotime = ticks / 10000;
     let tickDate = new Date(ticksToMicrotime);
 
