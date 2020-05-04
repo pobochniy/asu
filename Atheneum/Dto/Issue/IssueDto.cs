7de9fd66016cdbb672392c25a340143a1c3cbe3e
Jeer <phoenix_net@bk.ru>
2020-05-04 17:40:18 +0300

    fix Summary

--- Atheneum/Dto/Issue/IssueDto.cs
+++ Atheneum/Dto/Issue/IssueDto.cs
@@ -6,25 +6,49 @@ namespace Atheneum.Dto.Issue
 {
     public class IssueDto
     {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
         public long? Id { get; set; }
 
+        /// <summary>
+        /// Исполнитель
+        /// </summary>
         public Guid? Assignee { get; set; }
 
+        /// <summary>
+        /// Инициатор
+        /// </summary>
         [Required]
         public Guid Reporter { get; set; }
 
+        /// <summary>
+        /// Сводка
+        /// </summary>
         [Required]
         public string Summary { get; set; }
 
+        /// <summary>
+        /// Описание
+        /// </summary>
         [Required]
         public string Description { get; set; }
 
+        /// <summary>
+        /// Тип
+        /// </summary>
         [Required]
         public IssueTypeEnum Type { get; set; }
 
+        /// <summary>
+        /// Статус
+        /// </summary>
         [Required]
         public IssueStatusEnum Status { get; set; }
 
+        /// <summary>
+        /// Приоритет
+        /// </summary>
         [Required]
         public IssuePriorityEnum Priority { get; set; }
 
@@ -43,6 +67,9 @@ namespace Atheneum.Dto.Issue
         /// </summary>
         public DateTime? DueDate { get; set; }
 
+        /// <summary>
+        /// Ссылка на эпик
+        /// </summary>
         public int? EpicLink { get; set; }
     }
 }
