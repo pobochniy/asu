9839a3db0911de616a98bdbffc56797042612829
pobochniy <phoenix_net@bk.ru>
2020-09-18 17:34:21 +0300

    add AuthorizeRoles

---
+++ Web/Middleware/AuthorizeRoles.cs
@@ -0,0 +1,21 @@
+﻿using Atheneum.Enums;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Web.Middleware
+{
+    public class AuthorizeRoles : Attribute, IFilterFactory
+    {
+        public bool IsReusable => false;
+        public RoleEnum[] roles { get; set; }
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            var filter = serviceProvider.GetService(typeof(RolesValidation)) as RolesValidation;
+
+            filter.roles = roles;
+
+            return filter;
+        }
+    }
+}
---
+++ Web/Middleware/RolesValidation.cs
@@ -0,0 +1,43 @@
+﻿using Atheneum.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Web.Middleware
+{
+    public class RolesValidation : Attribute, IActionFilter
+    {
+        public RoleEnum[] roles { get; set; }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new StatusCodeResult(401);
+                return;
+            }
+
+            var userRoles = context.HttpContext.User.Claims
+                .Where(x => x.Type == ClaimTypes.Role)
+                .Select(x => (RoleEnum)int.Parse(x.Value))
+                .ToArray();
+
+            var isAllowed = false;
+            foreach (var el in roles)
+            {
+                if (userRoles.Contains(el))
+                {
+                    isAllowed = true;
+                    break;
+                }
+            }
+            if (!isAllowed) context.Result = new StatusCodeResult(403);
+        }
+    }
+}
