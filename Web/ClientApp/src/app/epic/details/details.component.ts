c933049519ad1e99167d05daf453e6c634364878
pobochniy <phoenix_net@bk.ru>
2021-02-27 22:02:01 +0300

    fix changes shared-user-name

--- Web/ClientApp/src/app/epic/details/details.component.ts
+++ Web/ClientApp/src/app/epic/details/details.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EpicApiService } from '../../shared/api/epic-api.service';
 import { UsersApiService } from '../../shared/api/users-api.service';
 import { IssuePriorityEnum } from '../../shared/enums/issue-priority.enum';
+import { EpicModel } from '../../shared/models/epic.model';
 import { UserProfileModel } from '../../shared/models/user-profile.model';
 import { ListComponent } from '../list/list.component';
-import { EpicModel } from '../../shared/models/epic.model';
-import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -16,32 +16,31 @@ import { DatePipe } from '@angular/common';
   providers: [EpicApiService, ListComponent, DatePipe]
 })
 export class DetailsComponent implements OnInit {
-  public epic: EpicModel;
+  public epic: EpicModel = new EpicModel();
   public profiles: UserProfileModel[];
   public issueTypes: { id: number; name: string }[] = [];
   public issueStatus: { id: number; name: string }[] = [];
-  public issuePriority: { id: number; name: string } [] = [];
+  public issuePriority: { id: number; name: string }[] = [];
 
   constructor(private service: EpicApiService
     , private userApiService: UsersApiService
     , private route: ActivatedRoute
     , private router: Router
     , public datepipe: DatePipe
-    , private cdRef: ChangeDetectorRef
   ) {
   }
 
   async ngOnInit() {
-    
+
     const id = +this.route.snapshot.paramMap.get('id');
 
     this.epic = await this.service.Details(id);
+
     for (var n in IssuePriorityEnum) {
       if (typeof IssuePriorityEnum[n] === 'number') {
         this.issuePriority.push({ id: <any>IssuePriorityEnum[n], name: n });
       }
     }
-    this.cdRef.detectChanges();
   }
 
   GetPriority(id: number) {
