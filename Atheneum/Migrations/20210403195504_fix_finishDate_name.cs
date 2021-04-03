ccf64e802e740853390ee1f56cc3b7af7afb4502
Nikolay4 <NikolayChurbakov92@mail.ru>
2021-04-03 23:04:29 +0300

    FinishtDate -> FinishDate

---
+++ Atheneum/Migrations/20210403195504_fix_finishDate_name.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Atheneum.Migrations
+{
+    public partial class fix_finishDate_name : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FinishtDate",
+                table: "Sprint",
+                newName: "FinishDate");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sprint_FinishtDate",
+                table: "Sprint",
+                newName: "IX_Sprint_FinishDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FinishDate",
+                table: "Sprint",
+                newName: "FinishtDate");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sprint_FinishDate",
+                table: "Sprint",
+                newName: "IX_Sprint_FinishtDate");
+        }
+    }
+}
